16bf1c715728715e79dd8ba0b6b22fb61ce1dd6d
Petryk Karina <karina.petryk.ir.2024@lpnu.ua>
2024-11-04 11:11:12 +0200

    apple changes from review

--- src/tree_model.py
+++ src/tree_model.py
@@ -46,13 +46,3 @@ class ArtificialTree:
     def __del__(self):
         print(f"ArtificialTree {self.__manufacturer} is being deleted")
 
-
-if __name__ == "__main__":
-    tree1 = ArtificialTree("TreeCorp", 150, 59.99, "Plastic")
-    tree2 = ArtificialTree("EverGreen", 180, 89.99, "PVC")
-    tree3 = ArtificialTree("EcoTrees", 200, 120.50, "Recycled Plastic")
-
-   
-    print(tree1)
-    print(tree2)
-    print(tree3)
